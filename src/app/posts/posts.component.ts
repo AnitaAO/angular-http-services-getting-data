e7f866e2470dd87450044d83047d3acdde199d41
AnitaAO <anitaisbeta@gmail.com>
2022-08-30 13:59:26 +0100

    updated

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,18 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
+interface Ipost {
+  body: '';
+  title: string;
+  id: number;
+  userId: number;
+}
+
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
 export class PostsComponent implements OnInit {
-  posts: any[];
+  private url: 'https://jsonplaceholder.typicode.com/posts';
+  posts: Ipost[];
 
   constructor(httpClient: HttpClient){
-    httpClient.get('https://jsonplaceholder.typicode.com/posts')
+    httpClient.get<Ipost[]>(this.url)
       .subscribe(response => {
-        this.posts = response.json();
+        this.posts = response;
       })
   };
 
